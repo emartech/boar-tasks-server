65a58a92ea8e75c4092b3a8de6b891e43b2df833
Somodi Viktor <somodi.viktor@gmail.com>
2016-02-03 11:26:10 +0100

    fix(run-command): breaking the task indeed if there's a test broken

--- tasks/run-command.js
+++ tasks/run-command.js
@@ -11,7 +11,11 @@ module.exports = function(gulp, config) {
     proc.stderr.pipe(process.stderr);
 
     proc.on('close', function (code) {
-      console.log('child process exited with code ' + code);
+      if (code === 0) {
+        cb();
+      } else {
+        cb(new Error(code));
+      }
     });
   };
 };
