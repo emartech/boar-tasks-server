0f5938e7c16cf0b82dbf63dec79f195b24880a2b
Somodi Viktor <somodi.viktor@gmail.com>
2016-02-20 09:49:14 +0100

    feat(run-command): add promise support

--- tasks/run-command.js
+++ tasks/run-command.js
@@ -2,21 +2,26 @@
 
 module.exports = function(gulp, config) {
   return function(command, cb) {
-    var _ = require('lodash');
-    var spawn = require('child_process').spawn;
-    var env = _.extend({}, process.env, config.server.environmentVariables);
-    var argv = process.argv.slice(2);
-    var proc = spawn('node', [command].concat(argv), { env: env });
-    proc.stdout.pipe(process.stdout);
-    proc.stdin.pipe(process.stdin);
-    proc.stderr.pipe(process.stderr);
+    return new Promise(function(resolve, reject) {
+      var _ = require('lodash');
+      var spawn = require('child_process').spawn;
+      var env = _.extend({}, process.env, config.server.environmentVariables);
+      var argv = process.argv.slice(2);
+      var proc = spawn('node', [command].concat(argv), { env: env });
+      proc.stdout.pipe(process.stdout);
+      proc.stdin.pipe(process.stdin);
+      proc.stderr.pipe(process.stderr);
 
-    proc.on('close', function (code) {
-      if (code === 0) {
-        cb();
-      } else {
-        cb(new Error(code));
-      }
+      proc.on('close', function (code) {
+        if (code === 0) {
+          if (cb) cb();
+          resolve();
+        } else {
+          var err = new Error(code);
+          if (cb) cb(cb);
+          reject(err);
+        }
+      });
     });
   };
 };
