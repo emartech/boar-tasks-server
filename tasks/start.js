51d3891ecd876a4d7c29590f1677f92be6dd4b9b
Somodi Viktor <somodi.viktor@gmail.com>
2016-02-20 09:43:11 +0100

    refactor(start): remove gulp

--- tasks/start.js
+++ tasks/start.js
@@ -1,23 +1,31 @@
 'use strict';
 
 module.exports = function(gulp, config) {
-  return function() {
-    var nodemon = require('gulp-nodemon');
+  return function(cb) {
+    var nodemon = require('nodemon');
     var notifier = require('node-notifier');
     var path = require('path');
+    var icon = path.join(__dirname, '..', 'boar.png');
+    var title = 'Boar tasks';
 
     nodemon({
       script: config.server.runnable,
       ext: 'js jade',
       watch: [config.build.distPath],
-      delay: 1,
-      env: config.server.environmentVariables,
-      nodeArgs: ['--harmony']
+      delay: 2,
+      env: config.server.environmentVariables
+    }).on('quit', function () {
+      notifier.notify({
+        title: title,
+        message: 'Server exited',
+        icon: icon
+      });
+      cb();
     }).on('restart', function() {
       notifier.notify({
-        title: 'Boar tasks',
+        title: title,
         message: 'Server restarted',
-        icon: path.join(__dirname, '..', 'boar.png')
+        icon: icon
       });
     });
   };
