18d18f2a12c5f79116b8649af832b807ad68fd04
Somodi Viktor <somodi.viktor@gmail.com>
2016-02-20 09:43:39 +0100

    feat(start): add promise support

--- tasks/start.js
+++ tasks/start.js
@@ -2,30 +2,33 @@
 
 module.exports = function(gulp, config) {
   return function(cb) {
-    var nodemon = require('nodemon');
-    var notifier = require('node-notifier');
-    var path = require('path');
-    var icon = path.join(__dirname, '..', 'boar.png');
-    var title = 'Boar tasks';
+    return new Promise(function(resolve) {
+      var nodemon = require('nodemon');
+      var notifier = require('node-notifier');
+      var path = require('path');
+      var icon = path.join(__dirname, '..', 'boar.png');
+      var title = 'Boar tasks';
 
-    nodemon({
-      script: config.server.runnable,
-      ext: 'js jade',
-      watch: [config.build.distPath],
-      delay: 2,
-      env: config.server.environmentVariables
-    }).on('quit', function () {
-      notifier.notify({
-        title: title,
-        message: 'Server exited',
-        icon: icon
-      });
-      cb();
-    }).on('restart', function() {
-      notifier.notify({
-        title: title,
-        message: 'Server restarted',
-        icon: icon
+      nodemon({
+        script: config.server.runnable,
+        ext: 'js jade',
+        watch: [config.build.distPath],
+        delay: 2,
+        env: config.server.environmentVariables
+      }).once('quit', function () {
+        notifier.notify({
+          title: title,
+          message: 'Server exited',
+          icon: icon
+        });
+        if (cb) cb();
+        resolve();
+      }).on('restart', function() {
+        notifier.notify({
+          title: title,
+          message: 'Server restarted',
+          icon: icon
+        });
       });
     });
   };
