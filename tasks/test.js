152c11925f4f254403f87d1f70505bd5e855cb1f
Somodi Viktor <somodi.viktor@gmail.com>
2016-02-20 09:47:33 +0100

    feat(test): add promise support

--- tasks/test.js
+++ tasks/test.js
@@ -2,33 +2,38 @@
 
 module.exports = function(gulp, config) {
   return function(cb) {
-    var requires = config.server.test.requires.reduce(function(requires, dependency) {
-      return requires.concat(['--require', dependency]);
-    }, []);
-    var flags = config.server.test.flags.reduce(function(flags, flag) {
-      var sliced = flag.split(' ');
-      sliced[0] = '--' +sliced[0];
-      return flags.concat(sliced);
-    }, []);
+    return new Promise(function(resolve, reject) {
+      var requires = config.server.test.requires.reduce(function(requires, dependency) {
+        return requires.concat(['--require', dependency]);
+      }, []);
+      var flags = config.server.test.flags.reduce(function(flags, flag) {
+        var sliced = flag.split(' ');
+        sliced[0] = '--' +sliced[0];
+        return flags.concat(sliced);
+      }, []);
 
-    var _ = require('lodash');
+      var _ = require('lodash');
 
-    var mochaPath = require('path').dirname(require.resolve('mocha')) + '/bin/mocha';
-    var command = [mochaPath].concat(flags).concat(requires).concat([config.server.path + '**/*.spec.js']);
-    var spawn = require('child_process').spawn;
-    var env = _.extend({}, process.env, config.server.test.environmentVariables);
-    var proc = spawn('node', command, { env: env });
+      var mochaPath = require('path').dirname(require.resolve('mocha')) + '/bin/mocha';
+      var command = [mochaPath].concat(flags).concat(requires).concat([config.server.path + '**/*.spec.js']);
+      var spawn = require('child_process').spawn;
+      var env = _.extend({}, process.env, config.server.test.environmentVariables);
+      var proc = spawn('node', command, { env: env });
 
-    proc.stdout.pipe(process.stdout);
-    proc.stdin.pipe(process.stdin);
-    proc.stderr.pipe(process.stderr);
+      proc.stdout.pipe(process.stdout);
+      proc.stdin.pipe(process.stdin);
+      proc.stderr.pipe(process.stderr);
 
-    proc.on('close', function (code) {
-      if (code === 0) {
-        cb();
-      } else {
-        cb(new Error(code));
-      }
+      proc.on('close', function (code) {
+        if (code === 0) {
+          if (cb) cb();
+          resolve();
+        } else {
+          var err = new Error(code);
+          if (cb) cb(err);
+          reject(err);
+        }
+      });
     });
   };
 };
