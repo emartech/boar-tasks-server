a2a502203c7b0ea6c3c80a2f60ae33f34167a987
Somodi Viktor <somodi.viktor@gmail.com>
2016-02-21 16:36:19 +0100

    feat(cli): run the given task with the given config (default: tasks.config.js)

--- cli.js
+++ cli.js
@@ -3,20 +3,24 @@
 const boarTasksServer = require('./');
 const co = require('co');
 const argv = require('yargs')
-    .usage('Usage: $0 -t [task]')
+    .usage('Usage: $0 -t [task] -c [config]')
+    .default('c', 'tasks.config.js')
     .demand(['t'])
     .argv;
 
-co(function* (){
-  console.log(`${argv.t} started!`);
-  const tasks = boarTasksServer.getTasks(null, {});
+const id = `Boar task '${argv.t})`;
+co(function* () {
+  console.time(id);
+  const config = require(`${process.cwd()}/${argv.c}`);
+  const tasks = boarTasksServer.getTasks(null, config);
   yield tasks.server[argv.t]();
-  console.log(`${argv.t} finished!`);
+  console.timeEnd(id);
 })
 .then(() => {
   process.exit(0);
 })
 .catch((ex) => {
-  console.log('something went wrong', ex.message, { stack: ex.stack });
+  console.timeEnd(id);
+  console.log(`${id} failed`, ex.message, { stack: ex.stack });
   process.exit(1);
 });
