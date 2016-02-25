01f9ff6c2000dc3124d9e92741c0d3afc947e40d
Somodi Viktor <somodi.viktor@gmail.com>
2016-02-25 06:40:26 +0100

    feat(cli): run multiple tasks (parallel or sequence)

--- cli.js
+++ cli.js
@@ -1,19 +1,27 @@
 'use strict';
 
 const boarTasksServer = require('./');
+const parallel = require('async-runner').parallel;
+const sequence = require('async-runner').sequence;
 const co = require('co');
 const argv = require('yargs')
-    .usage('Usage: $0 -t [task] -c [config]')
+    .usage('Usage: $0 [task1] [task2] -c [config]')
     .default('c', 'tasks.config.js')
-    .demand(['t'])
+    .boolean('p')
+    .boolean('s')
     .argv;
 
-const id = `Boar task '${argv.t}`;
+const id = `Boar task ${argv._}`;
+const taskIds = argv._;
+
 co(function* () {
+  console.log(`${id} started`);
   console.time(id);
   const config = require(`${process.cwd()}/${argv.c}`);
-  const tasks = boarTasksServer.getTasks(null, config);
-  yield tasks.server[argv.t]();
+  const boarTasks = boarTasksServer.getTasks(null, config);
+  const tasks = taskIds.map((taskId) => () => boarTasks.server[taskId]());
+  const runner = argv.parallel ? parallel : sequence;
+  yield runner(tasks);
   console.timeEnd(id);
 })
 .then(() => {
