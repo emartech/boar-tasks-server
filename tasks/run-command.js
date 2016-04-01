e73880ffaff2c3a61cbe739854c60deec50c7bb2
Gabor Balla <gabor.balla@emarsys.com>
2016-04-01 11:31:12 +0200

    feat(run-command): allow environment to override variables defined in tasks config

--- tasks/run-command.js
+++ tasks/run-command.js
@@ -4,7 +4,7 @@ module.exports = function(gulp, config) {
   return function(command, cb) {
     var _ = require('lodash');
     var spawn = require('child_process').spawn;
-    var env = _.extend({}, process.env, config.server.environmentVariables);
+    var env = _.extend({}, config.server.environmentVariables, process.env);
     var argv = process.argv.slice(2);
     var proc = spawn('node', [command].concat(argv), { env: env, stdio: 'inherit' });
 
