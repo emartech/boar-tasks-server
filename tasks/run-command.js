d1b077a9c4ef19bdddbc4444e19e6f16169c226f
Gabor Balla <gabor.balla@emarsys.com>
2016-04-01 11:30:24 +0200

    feat(run-command): better handling of stdio

--- tasks/run-command.js
+++ tasks/run-command.js
@@ -6,10 +6,7 @@ module.exports = function(gulp, config) {
     var spawn = require('child_process').spawn;
     var env = _.extend({}, process.env, config.server.environmentVariables);
     var argv = process.argv.slice(2);
-    var proc = spawn('node', [command].concat(argv), { env: env });
-    proc.stdout.pipe(process.stdout);
-    proc.stdin.pipe(process.stdin);
-    proc.stderr.pipe(process.stderr);
+    var proc = spawn('node', [command].concat(argv), { env: env, stdio: 'inherit' });
 
     proc.on('close', function (code) {
       if (code === 0) {
